44347a9201422adc2071dab97bc0d6c31fd8e3f0
josh <jkeife@gmail.com>
2021-12-06 22:28:58 -0700

    change path url

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 module "do_quake" {
-  source               = "git@github.com:gigawhat/terraform-digitalocean-quake.git?ref=v1.0.0"
+  source               = "github.com/gigawhat/terraform-digitalocean-quake?ref=v1.0.0"
   cloudflare_zone_name = "keife.org"
 }
