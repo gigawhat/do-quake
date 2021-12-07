c31cb91a70dc40cb261449402e1b6ea0592f459a
josh <jkeife@gmail.com>
2021-12-06 22:35:25 -0700

    add output

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ module "do_quake" {
   source               = "github.com/gigawhat/terraform-digitalocean-quake?ref=v1.0.0"
   cloudflare_zone_name = "keife.org"
 }
+
+output "quake_url" {
+  value = module.do_quake.quake_url
+}
